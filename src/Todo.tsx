a47d62e2168d098006aeb52dc14ae6b66dcf81bb
naotho17 <77154584+naotho17@users.noreply.github.com>
2021-11-24 11:54:25 +0900

    a

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,10 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+// Pickを使って、TodoTypeの中から使うプロパティだけを指定できる
+export const Todo = (
+  props: Pick<TodoType, "userId" | "title" | "completed">
+  // <Omit<Todotype, "id">
+) => {
   const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]";
   return <p>{`${completedMark}${title}(ユーザー:${userId})`}</p>;
